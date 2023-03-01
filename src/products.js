a162c57894bcb2f67374fd42598d4b108fa9dd2e
AshiqueBourg <ashique@bourgeontech.com>
2023-03-01 17:09:59 +0530

    1-3-23

---
+++ src/products.js
@@ -0,0 +1,26 @@
+import product1 from "./assets/products/1.jpg";
+import product2 from "./assets/products/2.jpg";
+import product3 from "./assets/products/3.png";
+
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Protein  Powder",
+    price: 999.0,
+    productImage: product3,
+  },
+  {
+    id: 2,
+    productName: "Protein powder 2",
+    price: 1899.0,
+    productImage: product1,
+  },
+  {
+    id: 3,
+    productName: "Track Sports 2",
+    price: 1299.0,
+    productImage: product2,
+  }
+ 
+];
